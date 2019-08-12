44ffcc2a81500b42ea966f386c5320a08a5d1503
Yeashai <yeashai.moskovitch@gmail.com>
2019-08-12 21:07:57 +0300

    first commit

---
+++ Contact/Connect_files/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "projectId": "connect-8fa78",
+  "appId": "1:288270137413:web:d9b7901764665a0b",
+  "databaseURL": "https://connect-8fa78.firebaseio.com",
+  "storageBucket": "connect-8fa78.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBVGFrt-A3Io5HLlGXhaHKfn0WJ-elC5T4",
+  "authDomain": "connect-8fa78.firebaseapp.com",
+  "messagingSenderId": "288270137413"
+});

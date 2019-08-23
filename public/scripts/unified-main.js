29a72f1431a49888edf89357d28077d8ae8a18ec
Yeashai <yeashai.moskovitch@gmail.com>
2019-08-23 12:36:52 +0300

    before changing buttons code

--- public/scripts/unified-main.js
+++ public/scripts/unified-main.js
@@ -1,6 +1,6 @@
 
 window.addEventListener("DOMContentLoaded", function () {
-  var pagesHTML = {
+  pagesHTML = {
     'userSignedOut': {
       // title: "Web App",
       'navbar-container': {
@@ -71,10 +71,10 @@ window.addEventListener("DOMContentLoaded", function () {
           `,
         'buttons': {
           'sign-out-btn': {
-            elementType: "button",
-            elementEventType: "click",
-            elementEventFunction: 'logUserOut',
-            elementEventFunctionParams: ''
+              buttontEventType: "click",
+              buttonEventFunction: logUserOut = function(){
+                  logUserOut();
+              }
           },
           'home-nav-btn': {
             elementType: "button",
@@ -195,10 +195,10 @@ window.addEventListener("DOMContentLoaded", function () {
             `,
           'buttons': {
             'sign-out-btn': {
-              elementType: "button",
-              elementEventType: "click",
-              elementEventFunction: 'logUserOut',
-              elementEventFunctionParams: ''
+              buttontEventType: "click",
+              buttonEventFunction: logUserOut = function(){
+                  logUserOut();
+              }
             },
             'home-nav-btn': {
               elementType: "button",
@@ -369,18 +369,15 @@ window.addEventListener("DOMContentLoaded", function () {
       for (const elementButtons in pageContent[elementID]["buttons"]) {
         document.getElementById(elementButtons).addEventListener(pageContent[elementID]["buttons"][elementButtons]["elementEventType"], function () {
           alert(`Button ${elementButtons} Clicked!`);
-          if (pageContent[elementID]["buttons"][elementButtons] === "loadPage") {
-            var 
-          }
+
         });
       }
     }
-
-    generate() {
-      this.generateElements(this.pageHTML, "navbar-container");
-      this.generateElements(this.pageHTML[this.homePage], "main-container");
-    }
+  generate() {
+    this.generateElements(this.pageHTML, "navbar-container");
+    this.generateElements(this.pageHTML[this.homePage], "main-container");
   }
+}
 
   //All code here
   userSignedIn = function () {
@@ -389,116 +386,116 @@ window.addEventListener("DOMContentLoaded", function () {
     //userSignedInPage.loadPage(pagesHTML.Home);
   };
 
-  userSignedOut = function () {
-    var userSignedOutPage = new appPage();
-    userSignedOutPage.loadPage(pagesHTML.userSignedOut);
-    // document.getElementById('account-details').textContent = 'null';
+userSignedOut = function () {
+  var userSignedOutPage = new appPage();
+  userSignedOutPage.loadPage(pagesHTML.userSignedOut);
+  // document.getElementById('account-details').textContent = 'null';
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
 
-    ui.start('#firebaseui-auth-container', uiConfig);
-  };
+  ui.start('#firebaseui-auth-container', uiConfig);
+};
 
-  logUserOut = function () {
-    // console.log("Clicked Sign Out!");
-    firebase.auth().signOut().then(function () {
-      // Sign-out successful.
-      console.log("Sign out Successfull!");
-      alert('Redirectin to Home Screen');
-      // window.location='/index.html';
-    }, function (error) {
-      // An error happened.
-      console.log("Sign out ***NOT*** Successfull!");
-    });
-  };
+logUserOut = function () {
+  // console.log("Clicked Sign Out!");
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+    console.log("Sign out Successfull!");
+    alert('Redirectin to Home Screen');
+    // window.location='/index.html';
+  }, function (error) {
+    // An error happened.
+    console.log("Sign out ***NOT*** Successfull!");
+  });
+};
 
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        var user = authResult.user;
-        var credential = authResult.credential;
-        var isNewUser = authResult.additionalUserInfo.isNewUser;
-        var providerId = authResult.additionalUserInfo.providerId;
-        var operationType = authResult.operationType;
-        // Do something with the returned AuthResult.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      signInFailure: function (error) {
-        // Some unrecoverable error occurred during sign-in.
-        // Return a promise when error handling is completed and FirebaseUI
-        // will reset, clearing any UI. This commonly occurs for error code
-        // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-        // occurs. Check below for more details on this.
-        return handleUIError(error);
-      },
-      uiShown: function () {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      var user = authResult.user;
+      var credential = authResult.credential;
+      var isNewUser = authResult.additionalUserInfo.isNewUser;
+      var providerId = authResult.additionalUserInfo.providerId;
+      var operationType = authResult.operationType;
+      // Do something with the returned AuthResult.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
     },
-    signInFlow: 'popup',
-    signInSuccessUrl: '/index.html',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: 'www.google.co.il',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function () {
-      window.location.assign('www.google.co.il');
+    signInFailure: function (error) {
+      // Some unrecoverable error occurred during sign-in.
+      // Return a promise when error handling is completed and FirebaseUI
+      // will reset, clearing any UI. This commonly occurs for error code
+      // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+      // occurs. Check below for more details on this.
+      return handleUIError(error);
+    },
+    uiShown: function () {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
     }
-  };
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: '/index.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: 'www.google.co.il',
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function () {
+    window.location.assign('www.google.co.il');
+  }
+};
 
-  initApp = function () {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        userSignedIn();
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var uid = user.uid;
-        var phoneNumber = user.phoneNumber;
-        var providerData = user.providerData;
-        /*user.getIdToken().then(function (accessToken) {
-          document.getElementById('account-details').textContent = JSON.stringify({
-            displayName: displayName,
-            email: email,
-            emailVerified: emailVerified,
-            phoneNumber: phoneNumber,
-            photoURL: photoURL,
-            uid: uid,
-            accessToken: accessToken,
-            providerData: providerData
-          }, null, '  ');
-        });*/
-      } else {
-        // User is signed out.
-        userSignedOut();
-        ui.start('#firebaseui-auth-container', uiConfig);
+initApp = function () {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      userSignedIn();
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var phoneNumber = user.phoneNumber;
+      var providerData = user.providerData;
+      /*user.getIdToken().then(function (accessToken) {
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: displayName,
+          email: email,
+          emailVerified: emailVerified,
+          phoneNumber: phoneNumber,
+          photoURL: photoURL,
+          uid: uid,
+          accessToken: accessToken,
+          providerData: providerData
+        }, null, '  ');
+      });*/
+    } else {
+      // User is signed out.
+      userSignedOut();
+      ui.start('#firebaseui-auth-container', uiConfig);
 
-      }
-    }, function (error) {
-      console.log(error);
-    });
-  };
+    }
+  }, function (error) {
+    console.log(error);
+  });
+};
 
-  initApp();
+initApp();
 
 });
 
